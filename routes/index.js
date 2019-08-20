f91ea9063c6710af0e1a6c537a6be03c14eed43d
wrc7 <wrodrigueschoaste@gmail.com>
2019-08-20 20:27:56 -0300

    Adiciona visualização de um unico post

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,22 @@ var postsService = require('../services/postsService');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-var posts = postsService.getPosts();
+    var posts = postsService.getPosts();
 
-res.render('index', {title: 'Blog', posts:posts});
+    res.render('index', {title: 'Blog', posts:posts});
 });
+
+router.get('/posts/:postId', function(req, res, next){
+    var postId = req.params.postId;
+
+    var posts = postsService.getPosts();
+    
+    var post = posts.filter((post) => post.id == postId)[0];
+
+    res.render('post', {title: post.title, post: post});
+
+
+
+})
 
 module.exports = router;
--- services/postsService.js
+++ services/postsService.js
@@ -1,16 +1,25 @@
 var getPosts = function(){
     var posts = [
         {
+            id: 1,
             title: 'Post 1',
             image: 'post1.jpg',
             description: 'Meu primeiro post',
             body: 'Meu primeiro post bla bla'
         },
         {
+            id: 2,
             title: 'Post 2',
             image: 'post2.jpg',
             description: 'Meu segundo post',
             body: 'Meu segundo post bla bla'
+        },
+        {
+            id: 3,
+            title: 'Sistema automatico de posts',
+            image: 'post2.jpg',
+            description: 'Novo sistema do blog',
+            body: 'Meu terceiro post bla bla'
         },
     ];
 
